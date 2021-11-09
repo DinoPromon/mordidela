ba45328eb8ac2e0be319e551f567af23f06823ff
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2021-11-09 15:46:55 -0300

    alterações no modal da página minha conta

--- components/Account/Modal/index.tsx
+++ components/Account/Modal/index.tsx
@@ -8,10 +8,13 @@ type Props = {
 const Modal: React.FC<Props> = (props) => {
   return (
     <Wrapper>
-      <h2>Modal Example</h2>
       <div>
         <span onClick={props.onClose}>&times;</span>
-        <p>Some text in the Modal..</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione atque consequuntur ipsum necessitatibus quaerat itaque, fuga, error quos nam officiis id, quae blanditiis eveniet deserunt architecto cum corrupti! Beatae, iure?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, provident at. Magni deleniti alias consectetur similique, harum dolor, ex tempora pariatur adipisci tempore officiis recusandae. Aperiam sed praesentium vitae quod.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione excepturi sint repellendus quidem voluptates, possimus ipsam minima, consequatur vero facere officiis nesciunt molestias odio? Est obcaecati unde maxime asperiores ad?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, accusamus exercitationem velit, nemo inventore dolor et recusandae dolores optio necessitatibus quam, laboriosam quo amet reprehenderit laudantium explicabo? Sunt, porro molestias!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus laborum ducimus, fugit, aliquid provident sunt, nemo cumque incidunt error molestiae consequatur voluptate perspiciatis placeat debitis minima adipisci qui a accusamus.</p>
       </div>
     </Wrapper>
   );
--- components/Account/Modal/styled.ts
+++ components/Account/Modal/styled.ts
@@ -1,31 +1,38 @@
 import styled from "styled-components";
+import { PURPLE } from "@utils/colors";
 
 const Wrapper = styled.div`
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
+  display: none;
+  position: fixed;
+  z-index: 1;
+  /* padding-top: 200px; */
   left: 0;
   top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0, 0, 0); /* Fallback color */
-  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+  width: 100%;
+  height: 100%; 
+  overflow: auto;
+  /* background-color: rgb(0, 0, 0);*/
+  background-color: rgba(0, 0, 0, 0.3);
 
   & > div {
-    position: relative;
+    /* position: relative; */
     background-color: white;
     margin: auto;
     padding: 20px;
-    border: 1px solid #888;
     width: 80%;
+    border-radius: 0.5rem;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
 
     & > span {
       font-size: 2rem;
-      position:absolute;
+      color: ${PURPLE};
+      position: absolute;
       right: 5px;
       top: 0;
+      cursor: pointer;
     }
   }
 `;
