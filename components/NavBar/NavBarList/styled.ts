9d6e4ec78a84608067713ec54540adc8ab8278a2
hironha <rafaelhirokato@gmail.com>
2021-11-24 15:32:13 -0300

    :lipstick: ajuste dos items na navbar

--- components/NavBar/NavBarList/styled.ts
+++ components/NavBar/NavBarList/styled.ts
@@ -3,12 +3,13 @@ import styled from "styled-components";
 const Wrapper = styled.ul`
   display: block;
   list-style: none;
-
+  padding-right: 2rem;
+  padding-left: 1rem;
   & > li {
     display: inline-block;
     color: white;
     font-weight: bold;
-    margin: 1rem 1.5rem;
+    margin: 1rem 0.5rem;
 
     &:nth-of-type(2) {
       float: right;
