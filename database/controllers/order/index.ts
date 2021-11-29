6be9811a16f4b73758002bf8996d29d2d328822e
hironha <rafaelhirokato@gmail.com>
2021-11-29 14:15:52 -0300

    :adhesive_bandage: consertado o preco entrega no pedido

--- database/controllers/order/index.ts
+++ database/controllers/order/index.ts
@@ -7,19 +7,17 @@ type PedidoData = Pick<Pedido, "id_usuario" | "tipo_pagamento" | "tipo_entrega" 
 export async function insertPedido(pedido: PedidoData) {
   const { id_cupom, id_usuario, tipo_entrega, tipo_pagamento, troco_para } = pedido;
   const cupom = await getCupomById(id_cupom);
-  const query =
-    tipo_entrega === "balcao" && cupom && cupom.tipo === "entrega"
-      ? "SELECT f_insert_pedido(?, ?, 0, ?, ?, ?)"
-      : "SELECT f_insert_pedido(?, ?, (SELECT id_endereco FROM endereco WHERE id_usuario = ?), ?, ?, ?)";
 
-  const result = (await mysql.query(query, [
-    id_cupom || null,
-    id_usuario,
-    id_usuario,
-    tipo_entrega,
-    tipo_pagamento,
-    troco_para || 0,
-  ])) as Pick<Pedido, "id_pedido">[];
+  const shouldRemoveDeliveryPrice = tipo_entrega === "balcao" && cupom && cupom.tipo === "entrega";
+  const query = shouldRemoveDeliveryPrice
+    ? "SELECT f_insert_pedido(?, ?, 0, ?, ?, ?)"
+    : "SELECT f_insert_pedido(?, ?, (SELECT id_endereco FROM endereco WHERE id_usuario = ?), ?, ?, ?)";
+
+  const params = shouldRemoveDeliveryPrice
+    ? [id_cupom || null, id_usuario, id_usuario, tipo_entrega, tipo_pagamento, troco_para || 0]
+    : [id_cupom || null, id_usuario, tipo_entrega, tipo_pagamento, troco_para || 0];
+
+  const result = (await mysql.query(query, params)) as Pick<Pedido, "id_pedido">[];
 
   const serializedResult = serialize(result);
   return Object.values(serializedResult[0])[0];
