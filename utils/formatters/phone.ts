a4ef073dbe92b3fc3e1cddfa09c6d6aacd1399f5
hironha <rafaelhirokato@gmail.com>
2021-12-03 10:51:41 -0300

    :adhesive_bandage: resolvido bug de formatação do numero de telefone quando apagava-se os 3 primeiros numeros iniciais

--- utils/formatters/phone.ts
+++ utils/formatters/phone.ts
@@ -2,7 +2,7 @@ import { clearPhoneNumber } from "@utils/transformation/phone";
 
 export function formatPhoneNumber(input: string) {
   const dddPattern = /^(\d{0,2})/g;
-  const first5Match = /^(\d{5})/g;
+  const number = /(\d{9})$/g;
   let formatedDDD = "";
   let phoneNumber = "";
   if (dddPattern.test(input)) {
@@ -10,20 +10,19 @@ export function formatPhoneNumber(input: string) {
     phoneNumber = input.slice(2);
   }
 
-  if (first5Match.test(phoneNumber)) {
-    phoneNumber = `${phoneNumber.match(first5Match)}-${phoneNumber.slice(5)}`;
+  if (number.test(phoneNumber)) {
+    phoneNumber = `${phoneNumber.slice(0, 5)}-${phoneNumber.slice(5, 9)}`;
   }
   return `${formatedDDD} ${phoneNumber}`;
 }
 
 export function phoneNumberChangeHandler(curNumber: string, prevNumber: string) {
   const failPattern = /(\D)$/;
-  const clearPattern = /[\s\(\)-]/g;
+  const clearNumber = clearPhoneNumber(curNumber);
 
-  if (curNumber.length < 2) return curNumber;
+  if (clearNumber.length < 2) return clearNumber;
   if (curNumber === prevNumber.slice(0, -1)) return curNumber;
 
-  const clearNumber = clearPhoneNumber(curNumber);
   if (failPattern.test(curNumber) || clearNumber.length > 11) return prevNumber;
 
   return formatPhoneNumber(clearNumber);
