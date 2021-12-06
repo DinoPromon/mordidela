c81a264bb7ed2dd532120f79990d66c274e4b1a2
hironha <rafaelhirokato@gmail.com>
2021-12-06 09:28:37 -0300

    :lipstick: adicionado função para fechar sidebar quando clica-se em algum link

--- components/NavBar/SideBar/SideBarList/index.tsx
+++ components/NavBar/SideBar/SideBarList/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
 
 import Wrapper from "./styled";
 
-const SideBarList: React.FC = () => {
+type Props = {
+  onClose: () => void;
+};
+
+const SideBarList: React.FC<Props> = (props) => {
+  const { onClose } = props;
+
   return (
     <Wrapper>
       <li>
-        <Link href="/">Início</Link>
+        <Link href="/">
+          <a onClick={onClose}>Início</a>
+        </Link>
       </li>
       <li>
-        <Link href="/cardapio">Cardápio</Link>
+        <Link href="/cardapio">
+          <a onClick={onClose}>Cardápio</a>
+        </Link>
       </li>
       <li>Sobre</li>
     </Wrapper>
--- components/NavBar/SideBar/index.tsx
+++ components/NavBar/SideBar/index.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import SideBarList from "./SideBarList";
 import useComponentVisible from "@hooks/useComponenteVisible";
 import Aside, { renderAnimation, unmountAnimation, SIDEBAR_ANIMATION_TIME } from "./styled";
-import SideBarList from "./SideBarList";
 
 type Props = {
   isShowingSidebar: boolean;
@@ -17,9 +17,7 @@ const SideBar: React.FC<Props> = (props) => {
   const [sidebarAnimation, setSidebarAnimation] = useState(renderAnimation);
   const { isShowingSidebar, setShowSideBar } = props;
 
-  const { ref: sidebarRef, isComponentVisible, setIsComponentVisible } = useComponentVisible(
-    isShowingSidebar
-  );
+  const { ref: sidebarRef, isComponentVisible, setIsComponentVisible } = useComponentVisible(isShowingSidebar);
 
   const closeHandler = () => {
     if (isComponentVisible) {
@@ -42,11 +40,11 @@ const SideBar: React.FC<Props> = (props) => {
         <FontAwesomeIcon icon={faTimes} size="lg" color="white" />
       </button>
       <Link href="/" passHref>
-        <a>
-          <Image src="/images/label_logo.png" alt="Logo do mordidela." width={100} height={80} layout="intrinsic"/>
+        <a onClick={closeHandler}>
+          <Image src="/images/label_logo.png" alt="Logo do mordidela." width={110} height={90} layout="intrinsic" />
         </a>
       </Link>
-      <SideBarList />
+      <SideBarList onClose={closeHandler} />
     </Aside>
   );
 };
