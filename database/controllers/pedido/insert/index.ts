27aa8bc73d44364ed4c699d2edd84268c4aadd97
hironha <rafaelhirokato@gmail.com>
2022-01-13 21:58:09 -0300

    :bug: arrumado bug de pedido sem cupom

--- database/controllers/pedido/insert/index.ts
+++ database/controllers/pedido/insert/index.ts
@@ -1,4 +1,3 @@
-import Cupom from "@models/cupom";
 import Pedido from "@models/pedido";
 import mysql, { serialize } from "database";
 import { CartPedido } from "@models/pedido";
@@ -7,7 +6,7 @@ import { getDeliveryPriceByUserId } from "@controllers/entrega";
 
 export async function insertPedido(pedido: CartPedido) {
   const { id_cupom, id_usuario, tipo_entrega, tipo_pagamento, troco_para } = pedido;
-  const cupom = (await getCupomById(id_cupom)) as Cupom;
+  const cupom = await getCupomById(id_cupom);
 
   const preco_entrega = await getDeliveryPriceByUserId(id_usuario);
   const shouldRemoveDeliveryPrice = tipo_entrega === "balcao" || (cupom && cupom.tipo === "entrega");
@@ -15,9 +14,11 @@ export async function insertPedido(pedido: CartPedido) {
     ? "SELECT f_insert_pedido(?, ?, (SELECT id_endereco FROM endereco WHERE id_usuario = ?), ?, ?, ?, ?)"
     : "SELECT f_insert_pedido(?, ?, (SELECT id_endereco FROM endereco WHERE id_usuario = ?), ?, ?, ?, ?)";
 
+  const codigoCupom = cupom ? cupom.codigo : undefined;
+
   const params = shouldRemoveDeliveryPrice
-    ? [cupom.codigo, id_usuario, id_usuario, 0.0, tipo_entrega, tipo_pagamento, troco_para || 0]
-    : [cupom.codigo, id_usuario, id_usuario, preco_entrega, tipo_entrega, tipo_pagamento, troco_para || 0];
+    ? [codigoCupom, id_usuario, id_usuario, 0.0, tipo_entrega, tipo_pagamento, troco_para || 0]
+    : [codigoCupom, id_usuario, id_usuario, preco_entrega, tipo_entrega, tipo_pagamento, troco_para || 0];
 
   const result = (await mysql.query(query, params)) as Pick<Pedido, "id_pedido">[];
 
