2c0f320f318d73a21fdc6686eb2081a9c634156a
hironha <rafaelhirokato@gmail.com>
2022-01-23 20:40:58 -0300

    :recycle: add prisma no método get

--- pages/api/order/index.ts
+++ pages/api/order/index.ts
@@ -1,6 +1,6 @@
 import { NextApiHandler } from "next";
 import { getSession } from "next-auth/client";
-
+import { Prisma } from "database";
 import { insertPedido } from "@controllers/pedido";
 import { CartPedido } from "@models/pedido";
 import { CartProduto } from "@models/produto";
@@ -10,11 +10,13 @@ import { insertAllProdutosFromPedido } from "@controllers/produto";
 const handler: NextApiHandler = async (req, res) => {
   try {
     const session = await getSession({ req });
-    if (!session) return res.status(401).json({ message: "É necessário autenticação para este endpoint." });
+    if (!session)
+      return res.status(401).json({ message: "É necessário autenticação para este endpoint." });
 
     switch (req.method) {
       case "POST":
         const { produtos, pedido } = req.body as { produtos: CartProduto[]; pedido: CartPedido };
+        console.log(pedido);
         if (pedido.id_usuario !== session.user.id_usuario)
           return res.status(403).json({ message: "Não pode inserir pedidos para outro usuário." });
 
@@ -29,7 +31,7 @@ const handler: NextApiHandler = async (req, res) => {
 
         return res.status(200).json({ success: true });
       case "GET":
-        const orders = await getAllPedidosFromUsuario(session.user.id_usuario);
+        const orders = await Prisma.pedido.findMany();
 
         return res.status(200).json(orders);
       default:
