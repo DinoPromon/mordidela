41bc03aa24f6601e41c105a3a9a40becc5ecc847
hironha <rafaelhirokato@gmail.com>
2022-01-25 14:50:48 -0300

    :lipstick: removido remoção de scroll quando em modal

--- components/shared/Modal/index.tsx
+++ components/shared/Modal/index.tsx
@@ -18,24 +18,21 @@ const Modal: React.FC<Props> = ({ onClose, children }) => {
   }
 
   useEffect(() => {
-    if (isComponentVisible)
-      document.body.setAttribute("style", `position: fixed; top: -${window.scrollY}px; left 0; right:0`);
-
     if (!isComponentVisible) {
       const timer = setTimeout(() => {
         onClose();
+
         return () => clearTimeout(timer);
       }, duration + 100);
     }
-
-    return () => {
-      document.body.setAttribute("style", "");
-      window.scrollTo(0, offsetY);
-    };
   }, [isComponentVisible, offsetY, onClose]);
 
   return (
-    <Wrapper isCloseAnimation={!isComponentVisible} duration={duration} shouldShowComponent={isComponentVisible}>
+    <Wrapper
+      isCloseAnimation={!isComponentVisible}
+      duration={duration}
+      shouldShowComponent={isComponentVisible}
+    >
       <div ref={modalRef as React.Ref<HTMLDivElement>} id="modal-content-container">
         <span onClick={buttonCloseHandler}>&times;</span>
         <Scroller>{children}</Scroller>
