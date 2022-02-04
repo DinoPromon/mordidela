6f57ba5c71d883ef1b5982eda95fb0e71b917b5c
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2022-02-04 19:31:12 -0300

    :lipstick: design do TextField

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-import { HomeContainer } from "./styled";
+import { HomeContainer, useStyles } from "./styled";
+import TextField from "@material-ui/core/TextField";
 import InputTextFormik from "@components/shared/InputTextFormik";
 
+
+
 const Home: React.FC = () => {
+  const classes = useStyles();
   return (
     <HomeContainer>
-      <InputTextFormik variant="outlined" />
+      <TextField
+        className={classes.root}
+        label="Nome"
+        variant="outlined"
+        color="primary"
+        required
+        fullWidth
+        autoComplete="off"
+      />
     </HomeContainer>
   );
 };
--- components/Home/styled.ts
+++ components/Home/styled.ts
@@ -1,6 +1,32 @@
 import styled from "styled-components";
 import { PURPLE } from "@utils/colors";
+import { makeStyles } from "@material-ui/styles";
+import { NONAME } from "dns";
 
 export const HomeContainer = styled.div`
   padding: 2rem;
 `;
+
+export const useStyles = makeStyles({
+  root: {
+    // Nome da label
+    '& .MuiFormLabel-root': {
+      color: "#3b3b3b",
+    },
+    // Bordas arredondadas
+    '& .MuiOutlinedInput-root': {
+      borderRadius: "5px",
+    },
+    // Entrada dos dados
+    '& .MuiInputBase-root': {
+      color: "#3b3b3b",
+    },
+    // Cor das bordas
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: "#bdbdbd",
+    },
+    '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+      /* borderColor: "red", */
+    },
+  },
+});
