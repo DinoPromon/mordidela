52ce5af7600d8e0f4accdf36ed0fd0e65da704d5
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2022-02-07 21:06:15 -0300

    :lipstick: estilização da página cupons

--- components/UserProfile/Cupom/index.tsx
+++ components/UserProfile/Cupom/index.tsx
@@ -1,11 +1,45 @@
 import React from "react";
-import { PageContainer } from "@components/shared";
-import { CouponsTitle } from "./styled";
+import { RiCoupon3Line } from "react-icons/ri";
+import { PageContainer, PageTitle } from "@components/shared";
+import { CupomContainer, CupomDataContainer, CupomTitle, CupomData } from "./styled";
+import { PINK, PURPLE } from "@utils/colors";
 
 const Coupons: React.FC = (props) => {
   return (
     <PageContainer>
-      <CouponsTitle>Cupons</CouponsTitle>
+      <PageTitle>Cupons</PageTitle>
+      <CupomContainer>
+        <CupomTitle>Cupons disponíveis</CupomTitle>
+        <CupomDataContainer>
+          <RiCoupon3Line size={40} color={PINK}/>
+          <CupomData>
+            <p>CODIGODOCUPOM - 15% de desconto</p>
+            <span>Válido até 07/02/2022</span>
+          </CupomData>
+        </CupomDataContainer>
+        <CupomDataContainer>
+          <RiCoupon3Line size={40} color={PINK}/>
+          <CupomData>
+            <p>ABCDEFGHIJKLMNOPQRST - 15% de desconto</p>
+            <span>Válido até sua utilização</span>
+          </CupomData>
+        </CupomDataContainer>
+        <CupomTitle>Cupons já utilizados</CupomTitle>
+        <CupomDataContainer>
+          <RiCoupon3Line size={40} color={PINK}/>
+          <CupomData>
+            <p>ABCDEFGHIJKLMNOPQRST - 15% de desconto</p>
+            <span>Utilizado no pedido Nº 587</span>
+          </CupomData>
+        </CupomDataContainer>
+        <CupomDataContainer>
+          <RiCoupon3Line size={40} color={PINK}/>
+          <CupomData>
+            <p>ABCDEFGHIJKLMNOPQRST - Frete grátis</p>
+            <span>Utilizado no pedido Nº 23494</span>
+          </CupomData>
+        </CupomDataContainer>
+      </CupomContainer>
     </PageContainer>
   );
 };
--- components/UserProfile/Cupom/styled.ts
+++ components/UserProfile/Cupom/styled.ts
@@ -1,10 +1,33 @@
 import { PURPLE, PINK } from "@utils/colors";
 import styled from "styled-components";
 
-export const CouponsTitle = styled.h1`
+export const CupomContainer = styled.div`
   display: flex;
-  font-size: 2.5rem;
-  margin: 1rem 0;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 500px;
+  margin: 0 auto;
+  padding-bottom: 50px;
+`;
+
+export const CupomTitle = styled.h3`
+  display: flex;
+  align-items: center;
+  justify-content:center;
   font-weight: bold;
-  color: ${PURPLE};
+  /* color: ${PURPLE}; */
+`;
+
+export const CupomDataContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const CupomData = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
 `;
