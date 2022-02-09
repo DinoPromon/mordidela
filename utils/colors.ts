10c4d7491140ce25ad0ce4abc1fc4cf14ef8b978
hironha <rafaelhirokato@gmail.com>
2022-02-08 21:13:02 -0300

    :lipstick: adicionado cores às constantes compartilhadas

--- utils/colors.ts
+++ utils/colors.ts
@@ -5,3 +5,7 @@ export const ORANGE = "#ff7c04";
 export const PURPLE = "rgba(130, 53, 206, 1)";
 
 export const PURPLE_TRANS = "rgba(130, 53, 206, 0.9)";
+
+export const ERROR_RED = "rgba(255, 0, 0, 0.8)";
+
+export const SUCCESS_GREEN = "rgba(27, 173, 19, 1)";
