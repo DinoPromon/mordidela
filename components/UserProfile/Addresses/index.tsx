91ede0b779c12e5b176036e59f970528276fe5cb
Luiz Gustavo Kobilacz <luizgustavokobilacz@gmail.com>
2022-02-09 21:02:49 -0300

    :lipstick: estilização da página endereços

--- components/UserProfile/Addresses/index.tsx
+++ components/UserProfile/Addresses/index.tsx
@@ -1,11 +1,36 @@
 import React from "react";
-import { PageContainer } from "@components/shared";
-import { AddressesTitle } from "./styled";
+import { CustomTextField, InputTextFormik, PageContainer, PageTitle } from "@components/shared";
+import { AddressesContainer, CustomInputsDesign, AddAddress } from "./styled";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import { FaPlusCircle } from "react-icons/fa";
+import { CustomFade } from "@components/shared";
+import { PINK, PURPLE } from "@utils/colors";
 
 const Addresses: React.FC = (props) => {
   return (
     <PageContainer>
-      <AddressesTitle>Endereços</AddressesTitle>
+      <PageTitle>Endereços</PageTitle>
+      <AddressesContainer>
+        <CustomInputsDesign>
+          <CustomTextField fullWidth autoComplete="off" variant="outlined" label="Logradouro *" />
+          <CustomTextField autoComplete="off" variant="outlined" label="Número *" />
+        </CustomInputsDesign>
+        <CustomTextField fullWidth autoComplete="off" variant="outlined" label="Bairro *" />
+        <CustomTextField fullWidth autoComplete="off" variant="outlined" label="Complemento" />
+        <AddAddress>
+          <FaPlusCircle size={16} color={PINK} />
+          <p>Adicionar endereço</p>
+        </AddAddress>
+        <span>
+          <Button size="large" variant="contained" color="secondary" type="submit">
+            Salvar alterações
+          </Button>
+        </span>
+{/*         <CustomFade triggerAnimation={values.delivery_type === "entrega"}>
+          <CartAddress addresses={addresses} isLoadingAddress={isLoadingAddress} />
+        </CustomFade> */}
+      </AddressesContainer>
     </PageContainer>
   );
 };
--- components/UserProfile/Addresses/styled.ts
+++ components/UserProfile/Addresses/styled.ts
@@ -1,10 +1,44 @@
 import { PURPLE, PINK } from "@utils/colors";
 import styled from "styled-components";
 
-export const AddressesTitle = styled.h1`
+export const AddressesContainer = styled.div`
   display: flex;
-  font-size: 2.5rem;
-  margin: 1rem 0;
-  font-weight: bold;
-  color: ${PURPLE};
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 600px;
+  margin: 0 auto;
+  padding-bottom: 50px;
+  width: 100%;
+
+  & > span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const CustomInputsDesign = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  width: 100%;
+
+  @media (max-width: 550px) {
+    flex-direction: column;
+    gap: 1rem;
+  }
+`;
+
+export const AddAddress = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: start;
+
+  &:hover {
+    cursor: pointer;
+    color: ${PINK};
+    text-decoration: underline;
+  }
 `;
