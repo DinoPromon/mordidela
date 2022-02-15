51495dc2af8002ff682323029c68f329302c259a
hironha <rafaelhirokato@gmail.com>
2022-02-14 21:43:23 -0300

    :recycle: remoção de código teste na home

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,5 +1,4 @@
-import Axios from "@api";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { HomeContainer } from "./styled";
 
@@ -8,35 +7,7 @@ type Teste = {
 };
 
 const Home: React.FC = () => {
-  const [testes, setTestes] = useState<Teste[]>([]);
-
-  useEffect(() => {
-    let isMounted = true;
-
-    fetchTest(isMounted);
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
-  async function fetchTest(isComponentMounted: boolean) {
-    const response = await Axios.get<string[]>("/teste");
-    if (isComponentMounted) setTestes(response.data.map((uuid) => ({ uuid } as Teste)));
-  }
-
-  return (
-    <HomeContainer>
-      {testes.map((teste, index) => (
-        <img
-          key={teste.uuid + index}
-          src={`/images/teste/${teste.uuid}.png`}
-          alt={"teste"}
-          loading="lazy"
-        />
-      ))}
-    </HomeContainer>
-  );
+  return <HomeContainer></HomeContainer>;
 };
 
 export default Home;
