f3492406ef46c37908e402d3db34633c2ec911d7
hironha <rafaelhirokato@gmail.com>
2022-03-05 15:51:55 -0300

    :recycle: reestruturação de código

--- database/controllers/order/createOrder/index.ts
+++ database/controllers/order/createOrder/index.ts
@@ -1,17 +1,12 @@
-import { AddRepo } from "@repository/add";
+import { Prisma } from "@database";
 import { throwError } from "@errors/index";
-import { CupomRepo } from "@repository/cupom";
-import { OrderRepo } from "@repository/order";
 import { CreateOrderValidator } from "./validator";
-import { UserCouponRepo } from "@repository/user-cupom";
-import { OrderProductRepo } from "@repository/order-product";
-import { OrderProductAddRepo } from "@repository/order-product-add";
-import { OrderProductFlavorRepo } from "@repository/order-product-flavor";
 
 import type ICupom from "@models/cupom";
 import type IPedido from "@models/pedido";
 import type IUsuario from "@models/usuario";
 import type IAdicional from "@models/adicional";
+import type IUsuarioCupom from "@models/usuario_cupom";
 import type IPedidoProduto from "@models/pedido_produto";
 import type { CartPedido } from "@models/pedido";
 import type { CartProduto } from "@models/produto";
@@ -37,7 +32,8 @@ export class CreateOrder {
   public async exec() {
     this.validator.validate();
 
-    const createdOrderId = await this.createOrder();
+    const { id_pedido: createdOrderId } = await this.createOrder();
+
     if (this.orderData.id_cupom) {
       await this.createCouponRelation(createdOrderId, this.orderData.id_cupom).catch((err) =>
         throwError("O-C", { customMessage: "Erro na criação da relação pedido e cupom" })
@@ -47,70 +43,97 @@ export class CreateOrder {
       throwError("O-C", { customMessage: "Erro na criação da relação pedido e produto" })
     );
 
-    await this.giveFidelityCoupon();
+    await this.giveFidelityCoupon().catch((err) => throwError("O-C"));
   }
 
   private async createCouponRelation(orderId: IPedido["id_pedido"], couponId: ICupom["id_cupom"]) {
-    const createdUserCoupon = await UserCouponRepo.create({
-      id_usuario: this.userId,
-      id_cupom: couponId,
-      id_pedido: orderId,
-      data_uso: new Date(),
-      foi_usado: true,
-    });
+    const createdUserCoupon = await Prisma.usuario_cupom
+      .create({
+        data: {
+          id_usuario: this.userId,
+          id_cupom: couponId,
+          id_pedido: orderId,
+          data_uso: new Date(),
+          foi_usado: true,
+        },
+      })
+      .catch((err) => throwError("O-C"));
 
-    return createdUserCoupon;
+    return createdUserCoupon as IUsuarioCupom;
   }
 
   private async giveFidelityCoupon() {
-    const fidelityCoupons = await CupomRepo.findAllFidelityCoupons();
-    const userOrdersAmount = await OrderRepo.countByUserId(this.userId);
+    const fidelityCoupons = (await Prisma.cupom.findMany({
+      where: {
+        fidelidade: true,
+      },
+    })) as ICupom[];
+
+    const userOrdersAmount = await Prisma.pedido.count({
+      where: {
+        id_usuario: this.userId,
+      },
+    });
 
     for (const coupon of fidelityCoupons) {
       if (userOrdersAmount % coupon.qtde_min_pedido === 0) {
-        return await UserCouponRepo.create({
-          id_cupom: coupon.id_cupom,
-          id_usuario: this.userId,
-          data_uso: null,
-          foi_usado: false,
-          id_pedido: null,
+        await Prisma.usuario_cupom.create({
+          data: {
+            id_cupom: coupon.id_cupom,
+            id_usuario: this.userId,
+            data_uso: null,
+            foi_usado: false,
+            id_pedido: null,
+          },
         });
+        return;
       }
     }
   }
 
   private async createProductRelations(orderId: IPedido["id_pedido"]) {
-    const allAdds = (await AddRepo.findAll()) as IAdicional[];
+    const allAdds = (await Prisma.adicional.findMany()) as IAdicional[];
 
     this.productsData.forEach(async (product) => {
       const selectedAdds = allAdds.filter((add) =>
         product.adicionais.find((orderAddId) => orderAddId === add.id_adicional)
       );
-      const createdOrderProduct = (await OrderProductRepo.create(
-        orderId,
-        product.id_produto,
-        product.quantidade,
-        product.observacao
-      )) as IPedidoProduto;
 
-      await OrderProductAddRepo.createMany(
-        createdOrderProduct.id_pedido_produto,
-        orderId,
-        product.id_produto,
-        selectedAdds
-      );
+      const createdOrderProduct = (await Prisma.pedido_produto.create({
+        data: {
+          id_pedido: orderId,
+          id_produto: product.id_produto,
+          quantidade: product.quantidade,
+          observacao: product.observacao,
+          preco_pedido: 0,
+        },
+      })) as IPedidoProduto;
 
-      await OrderProductFlavorRepo.createMany(
-        createdOrderProduct.id_pedido_produto,
-        orderId,
-        product.id_produto,
-        product.sabores
-      );
+      await Prisma.pedido_produto_adicional.createMany({
+        data: selectedAdds.map((add) => ({
+          id_pedido_produto: createdOrderProduct.id_pedido_produto,
+          id_pedido: orderId,
+          id_produto: product.id_produto,
+          id_adicional: add.id_adicional,
+          preco_adicional: add.preco,
+        })),
+      });
+
+      await Prisma.pedido_produto_sabor.createMany({
+        data: product.sabores.map((flavorId) => ({
+          id_pedido_produto: createdOrderProduct.id_pedido_produto,
+          id_pedido: orderId,
+          id_produto: product.id_produto,
+          id_sabor: flavorId,
+        })),
+      });
     });
   }
 
   private async createOrder() {
-    const createdOrderId = await OrderRepo.create(this.orderData);
-    return createdOrderId;
+    const createdOrderId = await Prisma.pedido.create({
+      data: this.orderData,
+    });
+    return createdOrderId as IPedido;
   }
 }
