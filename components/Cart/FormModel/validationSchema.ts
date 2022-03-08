6bb8658c610058a83561541dff8d7f4d98df8b86
hironha <rafaelhirokato@gmail.com>
2022-03-08 20:25:53 -0300

    :bug: correção no nome da variável delivery_type para deliveryType

--- components/Cart/FormModel/validationSchema.ts
+++ components/Cart/FormModel/validationSchema.ts
@@ -1,46 +1,48 @@
 import * as yup from "yup";
 import { transformPriceStringToNumber } from "@utils/transformation";
 
-export const useCartFormValidationSchema = (subTotalPrice: number) => {
+import { TipoEntrega } from "@models/pedido";
+import { TipoPagamento } from "@models/pedido";
 
+export const useCartFormValidationSchema = (subTotalPrice: number) => {
   const cartFormValidationSchema = yup.object().shape({
     deliveryType: yup.string().nullable().required("Selecione um tipo de entrega"),
     paymentType: yup.string().nullable().required("Selecione um tipo de pagamento"),
     addressId: yup
       .number()
       .nullable()
-      .when("delivery_type", (value) => {
-        if (value === "balcao") return yup.number().nullable();
+      .when("deliveryType", (value) => {
+        if (value === TipoEntrega.BALCAO) return yup.number().nullable();
         return yup.number().nullable().required("Selecione um endereço de entrega");
       }),
     needChange: yup
       .boolean()
       .nullable()
       .when("paymentType", (value) => {
-        if (value === "dinheiro")
+        if (value === TipoPagamento.DINHEIRO)
           return yup.string().nullable().required("Informe a necessidade de troco");
         return yup.string().nullable();
       }),
     paymentAmount: yup.string().when(["needChange", "paymentType", "deliveryPrice"], {
       is: (needChange: string, paymentType: string) => {
-        if (paymentType === "dinheiro" && needChange === "true") return true;
+        if (paymentType === TipoPagamento.DINHEIRO && needChange === "true") return true;
         return false;
       },
       then: yup.string().test("paymentAmount", "Valor insuficiente", (value, context) => {
-        const paymentAmount = value || "";
+        const stringPaymentAmount = value || "";
         const coupon = context.parent.coupon;
         if (coupon) {
-          const paymentAmountAsNumber = transformPriceStringToNumber(paymentAmount);
+          const numberPaymentAmount = transformPriceStringToNumber(stringPaymentAmount);
           if (
-            paymentAmountAsNumber <
+            numberPaymentAmount <
             context.parent.deliveryPrice + ((100 - coupon.valor_desconto) * subTotalPrice) / 100
           ) {
             return false;
           }
           return true;
         }
-        const paymentAmountAsNumber = transformPriceStringToNumber(paymentAmount);
-        if (paymentAmountAsNumber < context.parent.deliveryPrice + subTotalPrice) return false;
+        const numberPaymentAmount = transformPriceStringToNumber(stringPaymentAmount);
+        if (numberPaymentAmount < context.parent.deliveryPrice + subTotalPrice) return false;
         return true;
       }),
     }),
