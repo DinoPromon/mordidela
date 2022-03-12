5d1d4dc61a421b37fef6db8f57efb2c31efe80fb
hironha <rafaelhirokato@gmail.com>
2022-03-12 17:04:27 -0300

    :recycle: incremento no tempo de timout de requisição

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Axios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  timeout: 20 * 1000,
+  timeout: 120 * 1000,
   validateStatus: (status) => {
     return status >= 200 && status <= 300;
   },
